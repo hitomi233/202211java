26b199546638ab7723056aa0c0c6f017e3029ee3
hitomi233 <xietong320911@gmail.com>
2022-11-03 15:57:06 +0900

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.sql.SQLOutput;
+
+import static java.lang.System.out;
+ // int a =3;    //成员变量
+//static int b= 4; //静态变量
+public class Main {
+
+    public static void main(String[] args){
+int age= 18;//局部变量
+
+
+    }
+
+
+}
+
+
+
---
+++ src/com/company/hello.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * [HelloWorld!!]と標準出力クラスです。
+ */
+public class hello {
+    /**
+     * HelloWorldクラスをJavaコマンドから呼ばれた際に実行される処理です。
+     *
+     * @param args コマンドライン引数。今回は使いません。
+     */
+    public static void main(String[] args) {
+        Float flout1 = new Float(0.1);
+        Float flout2 = new Float(0.7);
+        System.out.println(flout1+flout2);
+
+    }
+}
+
